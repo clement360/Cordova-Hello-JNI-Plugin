550841d6470c6c4417c91942c731d80b5178e79c
garrydvaraza <mpano91@gmail.com>
2016-08-03 13:51:38 +0300

    add test.h test.h

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -1,12 +1,12 @@
 #import "HWPHello.h"
-
+#include "test.h"
 @implementation HWPHello
 
 - (void)greet:(CDVInvokedUrlCommand*)command
 {
-
+    //c_hello();
     NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+    NSString* msg = [NSString stringWithFormat: @"Hello, %@ and C-function says: %s", name, c_hello()];
 
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
